cd5fc639967183cf9073f766042f2d24c26e06df
salmankhan97 <72684302+salmankhan97@users.noreply.github.com>
2021-01-21 15:24:00 +0600

    Add files via upload

--- script.js
+++ script.js
@@ -13,62 +13,95 @@ let finalPriceTwo = priceTwo;
 document.getElementById("increment-one").addEventListener("click",function(){
     let quantityOne = document.getElementById("quantity-one").value;
     quantityOne++;
-    document.getElementById("quantity-one").value = quantityOne;
-    finalPriceOne = priceOne * quantityOne;
-    document.getElementById("price-one").innerText = finalPriceOne;
-    updateSubTotal();
+    if (quantityOne > 10){
+        alert("Maximum quantity per customer is 10");
+        quantityOne = 10;
+        updateFinalPriceOne(quantityOne);
+    }
+    else{
+        updateFinalPriceOne(quantityOne);
+    }
 });
-
 
 document.getElementById("decrement-one").addEventListener("click",function(){
     let quantityOne = document.getElementById("quantity-one").value;
     if (quantityOne != 0){
         quantityOne--;
-        document.getElementById("quantity-one").value = quantityOne;
-        finalPriceOne = priceOne * quantityOne;
-        document.getElementById("price-one").innerText = finalPriceOne;
-        updateSubTotal();
+        updateFinalPriceOne(quantityOne);
     }
 });
 
+
 document.getElementById("quantity-one").addEventListener("input",function(){
     let quantityOne = document.getElementById("quantity-one").value;
-    document.getElementById("quantity-one").value = quantityOne;
-    finalPriceOne = priceOne * quantityOne;
+    if (isNaN(quantityOne)){
+        alert("Please enter a valid Number");
+        quantityOne = 1;
+        updateFinalPriceOne(quantityOne);
+    }
+    else if (quantityOne > 10){
+        alert("Maximum quantity per customer is 10");
+        quantityOne = 10;
+        updateFinalPriceOne(quantityOne);
+    }
+    else{
+        updateFinalPriceOne(quantityOne);
+    }
+});
+
+function updateFinalPriceOne(x){
+    document.getElementById("quantity-one").value = x;
+    finalPriceOne = priceOne * x;
     document.getElementById("price-one").innerText = finalPriceOne;
     updateSubTotal();
-});
+}
 
 
 // cart two
 document.getElementById("increment-two").addEventListener("click",function(){
     let quantityTwo = document.getElementById("quantity-two").value;
     quantityTwo++;
-    document.getElementById("quantity-two").value = quantityTwo;
-    finalPriceTwo = priceTwo * quantityTwo;
-    document.getElementById("price-two").innerText = finalPriceTwo;
-    updateSubTotal();
-
+    if (quantityTwo > 50){
+        alert("Maximum quantity per customer is 50");
+        quantityTwo = 50;
+        updateFinalPriceTwo(quantityTwo);
+    }
+    else{
+        updateFinalPriceTwo(quantityTwo);
+    }
 });
 
 document.getElementById("decrement-two").addEventListener("click",function(){
     let quantityTwo = document.getElementById("quantity-two").value;
     if (quantityTwo != 0){
         quantityTwo--;
-        document.getElementById("quantity-two").value = quantityTwo;
-        finalPriceTwo = priceTwo * quantityTwo;
-        document.getElementById("price-two").innerText = finalPriceTwo;
-        updateSubTotal();
+        updateFinalPriceTwo(quantityTwo);
     }
 });
 
 document.getElementById("quantity-two").addEventListener("input",function(){
     let quantityTwo = document.getElementById("quantity-two").value;
-    document.getElementById("quantity-two").value = quantityTwo;
-    finalPriceTwo = priceTwo * quantityTwo;
+    if (isNaN(quantityTwo)){
+        alert("Please enter a valid Number");
+        quantityTwo = 1;
+        updateFinalPriceTwo(quantityTwo);
+    }
+    else if (quantityTwo > 50){
+        alert("Maximum quantity per customer is 50");
+        quantityTwo = 50;
+        updateFinalPriceTwo(quantityTwo);
+    }
+    else{
+        updateFinalPriceTwo(quantityTwo);
+    }
+});
+
+function updateFinalPriceTwo(x){
+    document.getElementById("quantity-two").value = x;
+    finalPriceTwo = priceTwo * x;
     document.getElementById("price-two").innerText = finalPriceTwo;
     updateSubTotal();
-});
+}
 
 
 
